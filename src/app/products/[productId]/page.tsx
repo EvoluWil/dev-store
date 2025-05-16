1fb786e7026261a8ac20d40b9e2f0cdc72912dae
Willian Rodrigues <willian.rsilva1994@gmail.com>
2025-05-16 18:21:09 -0300

    [fix] - Remove current product in the list component

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -2,12 +2,17 @@ import { Divider } from '@/components/atoms/divider.atom';
 import { ProductDetail } from '@/components/template/product-detail.template';
 import { ProductsListTemplate } from '@/components/template/products-list.template';
 
-export default function Home() {
+type ProductDetailParam = {
+  params: Promise<{ productId: string }>;
+};
+
+export default async function Products({ params }: ProductDetailParam) {
+  const { productId } = await params;
   return (
     <div className="px-8 max-w-screen-xl mx-auto w-full">
       <ProductDetail />
       <Divider />
-      <ProductsListTemplate />
+      <ProductsListTemplate productId={productId} />
     </div>
   );
 }
--- src/components/template/products-list.template.tsx
+++ src/components/template/products-list.template.tsx
@@ -1,14 +1,18 @@
 import { PRODUCTS } from '@/constants/products/products.constants';
 import { ProductCard } from '../organisms/product-card.organism';
 
-export const ProductsListTemplate = () => {
+type ProductDetailParam = {
+  productId: string;
+};
+
+export const ProductsListTemplate = ({ productId }: ProductDetailParam) => {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex flex-col gap-4">
         <h2 className="text-2xl font-bold">Related Products</h2>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-        {PRODUCTS.map((product) => (
+        {PRODUCTS.filter((item) => item.id !== productId).map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
