18b26c9d53ab868ca215f956f48d19be101f3bf3
Willian Rodrigues <willian.rsilva1994@gmail.com>
2025-05-16 14:47:49 -0300

    [feat] - Implements replace to product page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,6 @@
-'use client';
-
 import { PRODUCTS } from '@/constants/products/products.constants';
 import { redirect } from 'next/navigation';
 
-export default function Home() {
+export default async function NotFound() {
   return redirect(`/products/${PRODUCTS[0].id}`);
 }
